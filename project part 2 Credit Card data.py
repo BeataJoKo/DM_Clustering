d4e38a5257a12de375949472bf3ff2141fefd511
Draglus <74567577+Draglus@users.noreply.github.com>
2024-04-16 08:14:58 +0200

    Update project part 2 Credit Card data.py
    Updated so that you can see the silhouete score for each cluster

--- project part 2 Credit Card data.py
+++ project part 2 Credit Card data.py
@@ -5,8 +5,9 @@ Created on Tue Apr  9 16:35:11 2024
 @author: Mathi
 """
 
+
 import pandas as pd
-import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -85,8 +86,8 @@ print("The highest silhouette score found for KMeans model:", best_score_kmeans,
 
 #%%
 #Trying to find the best GMM plot
-best_model = None
-best_score = -1  # Initialize with a value lower than any possible silhouette score
+best_model_GMM = None
+best_score_GMM = -1  # Initialize with a value lower than any possible silhouette score
 for i in range(2,5):
     for _ in range(10):
         # Clustering with Gaussian Mixture Model
@@ -101,16 +102,16 @@ for i in range(2,5):
         score = silhouette_score(data, labels)
         print(score)
         # Check if the current model has a higher silhouette score than the best model so far
-        if score > best_score:
-            best_model = gmm
-            best_score = score
+        if score > best_score_GMM:
+            best_model_GMM = gmm
+            best_score_GMM = score
             clusters = i
             df['clusters_GMM']=labels
     
 
 
 # After n iterations, print the silhouette score of the best model
-print("The highest silhouette score found for Gaussian Mixture Model:", best_score,"Happens with ",clusters, "clusters")
+print("The highest silhouette score found for Gaussian Mixture Model:", best_score_GMM,"Happens with ",clusters, "clusters")
 #%%
 
 
@@ -164,7 +165,7 @@ plt.scatter(df_pca.iloc[:, 0], df_pca.iloc[:, 1],c=colors)
 
 plt.xlabel('principal component 1')
 plt.ylabel('principal component 2')
-plt.title('PCA med 2 komponenter')
+plt.title('KMEANS clustering using PC1 and PC2 as x and y axis')
 legend_elements = [
     plt.Line2D([0], [0], marker='o', color='w', label='Cluster 0', markerfacecolor='yellow', markersize=8),
     plt.Line2D([0], [0], marker='o', color='w', label='Cluster 1', markerfacecolor='blue', markersize=8),
@@ -183,30 +184,30 @@ plt.show()
 #We could also have done the kmeans model on just the PCA to reduce computation time.
 #This however comes with the loss of some of the explaniable variance.
 
-best_model_kmeans = None
-best_score_kmeans = -1  # Initialize with a value lower than any possible silhouette score
-x = df_pca["principal component 1"]
-y = df_pca["principal component 2"]
-data = list(zip(x, y))
+best_model_kmeans_pca = None
+best_score_kmeans_pca = -1  # Initialize with a value lower than any possible silhouette score
+x_ = df_pca["principal component 1"]
+y_ = df_pca["principal component 2"]
+data_pca = list(zip(x_, y_))
 
 for _ in range(50):
     # Clustering with Kmeans
     cluster = 3
     kmeans = KMeans(n_clusters=cluster,n_init=10)
-    kmeans.fit(data)
+    kmeans.fit(data_pca)
     
 
     
     # Compute silhouette score
-    score = silhouette_score(data, kmeans.fit_predict(data))
+    score = silhouette_score(data_pca, kmeans.fit_predict(data_pca))
     print(score)
     # Check if the current model has a higher silhouette score than the best model so far
     if score > best_score_kmeans:
-        best_model_kmeans = kmeans
-        best_score_kmeans = score
+        best_model_kmeans_pca = kmeans
+        best_score_kmeans_pca = score
 
 
-plt.scatter(x, y, c=best_model_kmeans.predict(data))
+plt.scatter(x_, y_, c=best_model_kmeans_pca.predict(data_pca))
 plt.title("Clusters made with the best kmeans model using only PC 1 and 2")
 legend_elements = [
     plt.Line2D([0], [0], marker='o', color='w', label='Cluster 0', markerfacecolor='turquoise', markersize=8),
@@ -220,4 +221,87 @@ plt.show()
 # After n iterations, print the silhouette score of the best model
 print("The highest silhouette score found for KMeans model:", best_score_kmeans)
 
+
+
+#%%
+sample_silhouette_values_kmeans = silhouette_samples(data, best_model_kmeans.predict(data))
+
+import matplotlib.cm as cm
+y_lower = 10
+n_clusters = 3
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+for i in range(3):
+    # Aggregate the silhouette scores for samples belonging to
+    # cluster i, and sort them
+    ith_cluster_silhouette_values = sample_silhouette_values_kmeans[df['cluster_Kmeans'] == i]
+
+    ith_cluster_silhouette_values.sort()
+
+    size_cluster_i = ith_cluster_silhouette_values.shape[0]
+    y_upper = y_lower + size_cluster_i
+
+    color = cm.nipy_spectral(float(i) / n_clusters)
+    ax1.fill_betweenx(
+        np.arange(y_lower, y_upper),
+        0,
+        ith_cluster_silhouette_values,
+        facecolor=color,
+        edgecolor=color,
+        alpha=0.7,
+    )
+
+    # Label the silhouette plots with their cluster numbers at the middle
+    ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+
+
+ax1.set_title("The silhouette plot for the various clusters in KMEANS.")
+ax1.set_xlabel("The silhouette coefficient values")
+ax1.set_ylabel("Cluster label")
+
+# The vertical line for average silhouette score of all the values
+ax1.axvline(x=best_score_kmeans, color="red", linestyle="--")
+
+ax1.set_yticks([])  # Clear the yaxis labels / ticks
+ax1.set_xticks([-0.2,-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+#%%
+
+sample_silhouette_values_gmm = silhouette_samples(data, best_model_GMM.predict(data))
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+for i in range(2):
+    # Aggregate the silhouette scores for samples belonging to
+    # cluster i, and sort them
+    ith_cluster_silhouette_values = sample_silhouette_values_gmm[df['clusters_GMM'] == i]
+
+    ith_cluster_silhouette_values.sort()
+
+    size_cluster_i = ith_cluster_silhouette_values.shape[0]
+    y_upper = y_lower + size_cluster_i
+
+    color = cm.nipy_spectral(float(i) / 2)
+    ax1.fill_betweenx(
+        np.arange(y_lower, y_upper),
+        0,
+        ith_cluster_silhouette_values,
+        facecolor=color,
+        edgecolor=color,
+        alpha=0.7,
+    )
+
+    # Label the silhouette plots with their cluster numbers at the middle
+    ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+
+ax1.set_title("The silhouette plot for the various clusters in GMM.")
+ax1.set_xlabel("The silhouette coefficient values")
+ax1.set_ylabel("Cluster label")
+
+# The vertical line for average silhouette score of all the values
+ax1.axvline(x=best_score_GMM, color="red", linestyle="--")
+
+ax1.set_yticks([])  # Clear the yaxis labels / ticks
+ax1.set_xticks([-0.5,-0.25, 0, 0.2, 0.4, 0.6, 0.8, 1])
 
